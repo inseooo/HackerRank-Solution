0217ed7414313c39d1c014043587af03554b4eb2
inseooo <inseo8080@gmail.com>
2024-02-25 17:24:50 +0900

    create solution

---
+++ SQL/01_Basic Select/Revising the Select Query II.sql
@@ -0,0 +1,16 @@
+/*
+URL :
+https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+
+Revising the Select Query II
+Language: MySQL
+
+*/
+
+select 
+    name 
+from 
+    city 
+where 
+    population >120000 
+    and countrycode ="USA"
