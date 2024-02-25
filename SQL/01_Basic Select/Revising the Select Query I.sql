6f21a1e5f2219d77a65cc99d2fa0571467224765
inseooo <inseo8080@gmail.com>
2024-02-25 17:08:12 +0900

    Create Soultion

---
+++ SQL/01_Basic Select/Revising the Select Query I.sql
@@ -0,0 +1,16 @@
+/*
+https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+Revising the Select Query I
+MySQL
+
+*/
+
+SELECT
+    *
+FROM 
+    CITY
+WHERE 
+    countrycode="USA"
+    And  POPULATION > 100000;
+
+
